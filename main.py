dc070384287c9a020011fe3ac9918069aba94df0
Conor <conordmcdevitt@gmail.com>
2023-07-07 19:01:03 -0400

    scrapes apartment information, next setting up email

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
+import time
 import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-craig_url = "https://vermont.craigslist.org/search/apa#search=1~gallery~0~0"
+#min price = 900 max price = 1200, distance = around burlington 
+craig_url = "https://vermont.craigslist.org/search/south-burlington-vt/apa?lat=44.4277&lon=-73.1534&max_price=1200&min_price=900&search_distance=7.1#search=1~gallery~0~0"
+driver = webdriver.Chrome()
 
-result = requests.get(craig_url).text
+driver.get(craig_url)
 
-soup = BeautifulSoup(result, "html.parser")
+time.sleep(5)
 
-apartments = soup.find(class_ = "cl-search-result")
+content = driver.page_source.encode('utf-8').strip()
+soup = BeautifulSoup(content, "html.parser")
 
-print(apartments)
+apartments = soup.find_all("div", class_ = "gallery-card")
+#amount of posts that are in burlington area 
+postings = int(soup.find("div", class_="count").get_text()[0])
 
+apartment_info = []
+
+#get info for each apartment 
+for apartment in apartments[:postings]:
+    print(apartment.find("span", class_="priceinfo").get_text())
+    print(apartment.find("a", class_="main").get("href"))
+    print(apartment.find("span", class_="label").get_text())
+    apartment_info.append({
+        "price": apartment.find("span", class_="priceinfo").get_text(),
+        "title": apartment.find("a", class_="main").get("href"),
+        "link": apartment.find("span", class_="label").get_text()
+    })  
+
+
+print(apartment_info)
 
