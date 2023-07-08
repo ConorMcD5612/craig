e56ef2292f8d0ee39a7d7f8a2c67d97bf9e37084
Conor <conordmcdevitt@gmail.com>
2023-07-08 14:52:05 -0400

    ingore test

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import time
 import requests
+import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+load_dotenv()
+email_pass = "icqzagqdzzltqdnb"
 #min price = 900 max price = 1200, distance = around burlington 
-craig_url = "https://vermont.craigslist.org/search/south-burlington-vt/apa?lat=44.4277&lon=-73.1534&max_price=1200&min_price=900&search_distance=7.1#search=1~gallery~0~0"
+
+CRAIG_URL = os.getenv('CRAIG_URL')
+
 driver = webdriver.Chrome()
 
-driver.get(craig_url)
+driver.get(CRAIG_URL)
 
 time.sleep(5)
 
@@ -29,7 +35,8 @@ for apartment in apartments[:postings]:
         "price": apartment.find("span", class_="priceinfo").get_text(),
         "title": apartment.find("a", class_="main").get("href"),
         "link": apartment.find("span", class_="label").get_text()
-    })  
+    }) 
+     
 
 
 print(apartment_info)
