ce4633168bfb8e6534ab9caf9904dfa1663c1823
Conor <conordmcdevitt@gmail.com>
2025-02-17 19:55:57 -0500

    need someway to get dynamic page

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ EMAIL_PASS = os.getenv("EMAIL_PASS")
 CRAIG_URL = os.getenv("CRAIG_URL")
 EMAIL = os.getenv("EMAIL")
 API = os.getenv("API")
+SENDER = os.getenv("SENDER")
 
-
+#have to use selenium to scrape dynamic data 
 
 
 # parse craigslist page into html
@@ -25,10 +26,13 @@ def get_apartments():
     #get html from craiglists page 
     page = requests.get(CRAIG_URL)
     soup = BeautifulSoup(page.content, "html5lib")
+    print(soup.prettify())
     
     #get apartments and that page 
     apartments = soup.find_all("li", class_="cl-static-search-result")
-    print(apartments)
+    #sort by date (past 24 hrs)
+    sorted_apartments = []
+   
     
     return apartments
 
@@ -36,57 +40,45 @@ def get_info(apartments):
     apartment_info = [{
         "price": apartment.find("div", class_="price").get_text(),
         "link": apartment.find("a").get("href"),
-        "title": apartment.find("div", class_="title").get_text()
+        "title": apartment.find("div", class_="title").get_text(),
     } for apartment in apartments]
+
 
     return apartment_info
 
 def create_email(apartment_info):
-    pass
-    
-    
+    html_content = ""
+    plain_content = ""
+    plain_msg = MIMEText(plain_content, "plain")
+    html_msg = MIMEText(html_content, "html")
+
+
+    em = MIMEMultipart("alternative")
+    em["From"] = SENDER
+    em["To"] = EMAIL
+    em["Subject"] = "New Apartment Listing"
+    em.attach(plain_msg)
+    em.attach(html_msg)
+
+    for apartment in apartment_info:
+        html_content += f'<h1><a href="{apartment["link"]}">{apartment["title"]}</a></h1><h2>{apartment["price"]}</h2><br>'
+        plain_content += (
+            f'{apartment["title"]}  {apartment["price"]} \n {apartment["link"]}\n'
+        )
+    return em
+
+#don't want everything returning should just be past 24 hrs 
+def send_email(em):
+
+    with smtplib.SMTP("live.smtp.mailtrap.io", 587) as server:
+        server.starttls()
+        server.login("api", API)
+        server.sendmail("hello@demomailtrap.com", EMAIL, em.as_string())
+        
     # get info for each apartment
     
 
 
 
-html_content = ""
-plain_content = ""
+
 apartment_info = get_info(get_apartments())
-# make strings
-for apartment in apartment_info:
-    html_content += f'<h1><a href="{apartment["link"]}">{apartment["title"]}</a></h1><h2>{apartment["price"]}</h2><br>'
-    plain_content += (
-        f'{apartment["title"]}  {apartment["price"]} \n {apartment["link"]}\n'
-    )
-
-
-plain_msg = MIMEText(plain_content, "plain")
-html_msg = MIMEText(html_content, "html")
-
-
-em = MIMEMultipart("alternative")
-em["From"] = EMAIL
-em["To"] = EMAIL
-em["Subject"] = "New Apartment Listing"
-em.attach(plain_msg)
-em.attach(html_msg)
-
-
-context = ssl.create_default_context()
-message = f"""\
-Subject: Hi Mailtrap
-To: {EMAIL}
-From: hello@demomailtrap.com
-
-This is a test e-mail message."""
-
-with smtplib.SMTP("live.smtp.mailtrap.io", 587) as server:
-    server.starttls()
-    server.login("api", API)
-    server.sendmail("hello@demomailtrap.com", EMAIL, message)
-
-
-
-
-# create mail object
